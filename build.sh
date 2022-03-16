910104c453fafb3a563361eb96a3b45db8748ab3
blacksky <blacksky3@tuta.io>
2022-03-16 17:29:19 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,23 +15,23 @@ cd lld-git && makepkg -si --noconfirm --nocheck && cd ..
 
 cd polly-git && makepkg -si --noconfirm --nocheck && cd ..
 
-cd compiler-rt-git && makepkg -si --noconfirm --nocheck && cd ..
+cd compiler-rt-git && makepkg -si --noconfirm && cd ..
 
 cd clang-git && makepkg -si --noconfirm --nocheck && cd ..
 
-cd lldb-git && makepkg -si --noconfirm --nocheck && cd ..
+cd lldb-git && makepkg -si --noconfirm && cd ..
 
 cd spirv-llvm-translator-git && makepkg -si --noconfirm --nocheck && cd ..
 
-cd libclc-git && makepkg -si --noconfirm --nocheck && cd ..
+cd libclc-git && makepkg -si --noconfirm && cd ..
 
 cd libc++-git && makepkg -si --noconfirm --nocheck && cd ..
 
-cd openmp-git && makepkg -si --noconfirm --nocheck && cd ..
+cd openmp-git && makepkg -si --noconfirm && cd ..
 
-cd lib32-llvm-git && makepkg -si --noconfirm --nocheck && cd ..
+cd lib32-llvm-git && makepkg -si --noconfirm && cd ..
 
-cd lib32-clang-git && makepkg -si --noconfirm --nocheck && cd ..
+cd lib32-clang-git && makepkg -si --noconfirm && cd ..
 
 # make a copy of every pkg in package-$(date -I)/ dir
 
